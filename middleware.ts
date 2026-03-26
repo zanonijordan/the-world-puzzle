647c07f164d41b85cb278de19e59627c6be28aaf
zanonijordan <zanoni.jordan07@gmail.com>
2026-03-26 11:44:19 +0000

    `Added secureCookie option to getToken function`

--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,7 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
     secret: process.env.AUTH_SECRET ?? process.env.NEXTAUTH_SECRET,
+    secureCookie: req.nextUrl.protocol === "https:",
   });
 
   if (!token) {
